5955e1099611f4a68500cd3377e75e4b84bfabcd
Talal <talalothmanse@gmail.com>
2022-09-08 03:36:42 +0800

    -fixed import issue

--- pages/submit-image.js
+++ pages/submit-image.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 // import styles from '../styles/upload-image.module.css'
-import styles from '../../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
